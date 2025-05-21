85f428fe5b2fa15025553e7f37e5dbe26f95da68
juanpaez00 <juan@replo.app>
2025-05-21 15:25:37 -0300

    .

--- src/app/api/drive/videos/route.ts
+++ src/app/api/drive/videos/route.ts
@@ -92,7 +92,7 @@ export async function GET(request: NextRequest) {
             ? "Biofísica"
             : subjectName === "biologia"
             ? "Biología"
-            : subjectName === "bioquímica"
+            : subjectName === "bioquimica"
             ? "Bioquímica"
             : "Fisiología",
         sections,
